23266927c5094cb947f0dbd5b09ab29194948299
Serhii Bulba <sergeybulyba2016@gmail.com>
2022-05-09 10:38:53 +0300

    commit2

---
+++ Lesson8_HomeTask.py
@@ -0,0 +1,20 @@
+## GAME "Rock Paper Scissors Lizard Spock"
+
+from lib import win_rules, get_user_choice, get_computer_choice, get_winner, make_message
+
+def main():
+    """MAIN FUNCTION FOR THE GAME 'Rock Paper Scissors Lizard Spock'
+    """
+    for i in range(1, 4):
+        print(f'Try {i}')
+        user_choice = get_user_choice()
+
+        computer_choice = get_computer_choice()
+
+        winner = get_winner(user_choice, computer_choice)
+
+        msg = make_message(user_choice, computer_choice, winner)
+
+        print(msg)
+
+main()
