43708d2b96c4d178c8940a5938f36742e502698d
Serhii Bulba <sergeybulyba2016@gmail.com>
2022-05-17 22:38:52 +0300

    commits

--- ClassWork_lesson4.py
+++ ClassWork_lesson4.py
@@ -1,99 +1,99 @@
-# # Teacher suggest us use the cycle "for" for working under collections (list, tuple)
-# for element in (1, 2, 3):
-#     print(element)
+# Teacher suggest us use the cycle "for" for working under collections (list, tuple)
+for element in (1, 2, 3):
+    print(element)
 
-### Data Type "dictionary"
-# my_dict = {0: 'a', 1000.1: 'b', 100: 'c'}
-# print(my_dict)
-# print(my_dict[0])
-# print(my_dict[1000.1])
+## Data Type "dictionary"
+my_dict = {0: 'a', 1000.1: 'b', 100: 'c'}
+print(my_dict)
+print(my_dict[0])
+print(my_dict[1000.1])
 
 
 # search the data in list is provided through the 'index', but in dictionary - by the 'key'
 # the data: 0, 100.1 and 100 are the 'key' of the dictionary
 
 # what data types could be the keys to ductionary? They are: int, float, string, bool, tuple and None
-# # By the way  - fata type of key is always static data
-# my_dict = {
-#     0: 'a',
-#     1000.1: 'b',
-#     'string': 'c',
-#     True: '1500',
-#     (1, 2): '125fgds',
-#     None: '45ff',
-#     'Dictionary': {1: 'gfd'}
-#     }
+# By the way  - fata type of key is always static data
+my_dict = {
+    0: 'a',
+    1000.1: 'b',
+    'string': 'c',
+    True: '1500',
+    (1, 2): '125fgds',
+    None: '45ff',
+    'Dictionary': {1: 'gfd'}
+    }
 
-# print(my_dict)
-# print(my_dict['Dictionary'])
+print(my_dict)
+print(my_dict['Dictionary'])
 
-# # If I want to add the new key with value into dictionary:
-# my_dict['name'] = 'new strint'
-# print(my_dict)
+# If I want to add the new key with value into dictionary:
+my_dict['name'] = 'new strint'
+print(my_dict)
 
-# # If need to rewrite the value, I need to push the request with the same key but with different value:
-# my_dict['name'] = ['fd', 1, [1, 2]]
-# print(my_dict)
+# If need to rewrite the value, I need to push the request with the same key but with different value:
+my_dict['name'] = ['fd', 1, [1, 2]]
+print(my_dict)
 
-# # I can derive the data from the list, which is included into the dictionary:
-# print(my_dict['name'])
-# print(my_dict['name'][0])
-# # so based on the above mentioned: all operations with lists and strings are available inside the dectionary
+# I can derive the data from the list, which is included into the dictionary:
+print(my_dict['name'])
+print(my_dict['name'][0])
+# so based on the above mentioned: all operations with lists and strings are available inside the dectionary
 
-# ## We can use the cycle 'For' for dictionary:
-# for i in my_dict:
-#     print(i)
-# # we received the key - so, to retreive the keys fron the dictionary we can use the cycle "for"
+## We can use the cycle 'For' for dictionary:
+for i in my_dict:
+    print(i)
+# we received the key - so, to retreive the keys fron the dictionary we can use the cycle "for"
 
-# my_dict = {
-#     0: 'a',
-#     1000.1: 'b',
-#     'string': 'c',
-#     True: '1500',
-#     (1, 2): '125fgds',
-#     None: '45ff',
-#     'Dictionary': {1: 'gfd'}
-#     }
-# # How we can retrive the values fron the dictionary? There are several approaches:
+my_dict = {
+    0: 'a',
+    1000.1: 'b',
+    'string': 'c',
+    True: '1500',
+    (1, 2): '125fgds',
+    None: '45ff',
+    'Dictionary': {1: 'gfd'}
+    }
+# How we can retrive the values fron the dictionary? There are several approaches:
 
-# Approach 1:
-# for i in my_dict:
-#     print(my_dict[i])
+#Approach 1:
+for i in my_dict:
+    print(my_dict[i])
 
-# Approach 2: we can use methods 'key' and 'value' for the dictionary
-# for key in my_dict.keys():
-#     print(key)
+#Approach 2: we can use methods 'key' and 'value' for the dictionary
+for key in my_dict.keys():
+    print(key)
 
-# for value in my_dict.values():
-#     print(value)
+for value in my_dict.values():
+    print(value)
 
-# for item in my_dict.items():
-#     print(item)
+for item in my_dict.items():
+    print(item)
 
-# for key, value in my_dict.items():
-#     print(key)
-#     print(value)
+for key, value in my_dict.items():
+    print(key)
+    print(value)
 
 ### Working with dynamic and static data
 # Sample of dynamic data - there are changes when cut data from a particular chunk of memory and store it in another
-# var1 = 1
-# print(id(var1))  # whithin a peace of program the chunk is '1433771704560'
-# var1 += 1
-# print(id(var1))  # if make calculation then chunk is '1294123663632', so it is dynamic from operation to operation
+var1 = 1
+print(id(var1))  # whithin a peace of program the chunk is '1433771704560'
+var1 += 1
+print(id(var1))  # if make calculation then chunk is '1294123663632', so it is dynamic from operation to operation
 
 
 # Sample of static data - No changes when cut data from a particular chunk of memory and store it in another
-# list1 = [1, 2]
-# print(id(list1))   # whithin a peace of program the chunk is '2805489587136'
-# list1.append([2, 3])
-# print(id(list1))   # if make calculation then chunk is still '2805489587136', so it is static from operation to operation
+list1 = [1, 2]
+print(id(list1))   # whithin a peace of program the chunk is '2805489587136'
+list1.append([2, 3])
+print(id(list1))   # if make calculation then chunk is still '2805489587136', so it is static from operation to operation
 
 # another sample with string
-# my_str = 'fda'
-# print(id(my_str))
-# my_str.replace('f', 'N')
-# print(id(my_str))
-# #** The key in the dictionary could be only static data
+my_str = 'fda'
+print(id(my_str))
+my_str.replace('f', 'N')
+print(id(my_str))
+#** The key in the dictionary could be only static data
 
 ### How to remove the 'key' with 'value' from the dictionary:
 my_dict = {
@@ -102,34 +102,34 @@ my_dict = {
     'string': 'c'
         }
 
-# my_dict.pop(True)
-# print(my_dict)
+my_dict.pop(True)
+print(my_dict)
 
-### How to add the 'key' with 'value' from the dictionary:
-# my_dict['new key'] = 'new value'
-# print(my_dict)
+## How to add the 'key' with 'value' from the dictionary:
+my_dict['new key'] = 'new value'
+print(my_dict)
 
 
 ### How to update the 'key' with 'value' from the dictionary (in this case the key/value is updated unlikely to the case above):
-# my_dict.update({'new key': 'new value'})
-# print(my_dict)
-# my_dict.update({'new key': '1'})
-# print(my_dict)
+my_dict.update({'new key': 'new value'})
+print(my_dict)
+my_dict.update({'new key': '1'})
+print(my_dict)
 
-# print(my_dict)
+print(my_dict)
 
 ## ! We cannot multiply the dictionary by some value (dict * value) !
 
-# ### SET data type
-# # set contains only unick data
-# # there is not any order in data set, no priorities, no indexes and no keys
-# # we can add data to set but whithout any order. We can remove data
-# # Set can be used for analytic mathematics. Also we can infiltrate the uniq values through the set
-# my_set = {2, 1, 3, 5, 'a'}
-# my_set.add('f')
-# print(my_set)
-# my_set.remove('a')
-# print(my_set)
+### SET data type
+# set contains only unick data
+# there is not any order in data set, no priorities, no indexes and no keys
+# we can add data to set but whithout any order. We can remove data
+# Set can be used for analytic mathematics. Also we can infiltrate the uniq values through the set
+my_set = {2, 1, 3, 5, 'a'}
+my_set.add('f')
+print(my_set)
+my_set.remove('a')
+print(my_set)
 
 
 # ## How to ask if the particular element exists in the dictionary, set?
